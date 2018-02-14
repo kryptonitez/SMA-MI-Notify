147774781667bd93717c8587908b3dccad836f9f
user136742 <user136742@74-80-245-28.ic2net.net>
2018-02-14 15:32:19 -0800

    Notify initial commit

---
+++ SMA MI Notify/FormData.swift
@@ -0,0 +1,29 @@
+//
+//  FormData.swift
+//  SMA MI Notify
+//
+//  Created by user136742 on 2/14/18.
+//  Copyright © 2018 SMA. All rights reserved.
+//
+
+import UIKit
+
+class FormData {
+
+//MARK:Properties
+
+var question: String
+var response: String
+    
+    //MARK: Initialization
+    
+    init?(question: String, response: String) {
+    // Initialization should fail if there is no question
+        if question.isEmpty {
+            return nil
+        }
+        self.question = question
+        self.response = response
+    }
+
+}
---
+++ SMA MI Notify/Question.swift
@@ -0,0 +1,24 @@
+//
+//  Question.swift
+//  SMA MI Notify
+//
+//  Created by user136742 on 2/14/18.
+//  Copyright © 2018 SMA. All rights reserved.
+//
+
+import UIKit
+
+class Question: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
