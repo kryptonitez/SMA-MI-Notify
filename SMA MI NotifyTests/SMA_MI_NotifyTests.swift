485b0d91ee6a81a11a57b812f2a3e03d2c8e756f
user136742 <user136742@74-80-245-28.ic2net.net>
2018-02-14 10:13:00 -0800

    Initial Commit

---
+++ SMA MI NotifyTests/SMA_MI_NotifyTests.swift
@@ -0,0 +1,36 @@
+//
+//  SMA_MI_NotifyTests.swift
+//  SMA MI NotifyTests
+//
+//  Created by user136742 on 2/14/18.
+//  Copyright © 2018 SMA. All rights reserved.
+//
+
+import XCTest
+@testable import SMA_MI_Notify
+
+class SMA_MI_NotifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
